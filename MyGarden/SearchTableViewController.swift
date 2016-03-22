ca2c0ebac9bb0db22dc90e5e643f8fc6f20dcd43
Pascal Reitermann <pascalreitermann@pascals-air.fritz.box>
2016-03-22 13:17:46 +0100

    Such Funktion implementiert

---
+++ MyGarden/SearchTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SearchTableViewController.swift
+//  MyGarden
+//
+//  Created by Pascal Reitermann on 22.03.16.
+//  Copyright © 2016 Pascal Reitermann. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    let plants = ["Basilikum", "Petersile", "Schnittlauch", "Erdbeeren"]
+    var filteredArray = [String]()
+    var resultSearchController = UISearchController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.resultSearchController = UISearchController(searchResultsController: nil)
+        self.resultSearchController.searchResultsUpdater = self
+        self.resultSearchController.dimsBackgroundDuringPresentation = false
+        self.resultSearchController.searchBar.sizeToFit()
+        
+        self.tableView.tableHeaderView = self.resultSearchController.searchBar
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if self.resultSearchController.active
+        {
+            return self.filteredArray.count
+        } else {
+            return self.plants.count
+        }
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell?
+        
+        if self.resultSearchController.active
+        {
+            cell!.textLabel?.text = self.filteredArray[indexPath.row]
+        } else {
+            cell!.textLabel?.text = self.plants[indexPath.row]
+        }
+        
+        return cell!
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        self.filteredArray.removeAll(keepCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        
+        let array = (self.plants as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        
+        self.filteredArray = array as! [String]
+        
+        self.tableView.reloadData()
+        
+    }
+    
+}
